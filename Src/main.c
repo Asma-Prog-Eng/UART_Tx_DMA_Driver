f3c1b1d6ea4b135aeb5bc8a1fea143e41de3499e
Asma-Prog-Eng <asmaenim@gmail.com>
2025-07-23 09:22:44 +0200

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -1,18 +1,16 @@
-/* Bare metal implementation of a USART DMA transmitter Driver
- * with configurable USART peripheral  ( USART1, USART2, USART6)
- * Features :
- * Tx Pin : PA2
- * Configurable Baud rate : {4800,9600,115200}
- * DMA interrupt is raised if reset button is pressed
- * If message is transmitted than two LEDS (LED4 and LED6) are toggled accordingly
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : Asma Askri
+ * @brief          : Main program body
+ ******************************************************************************
+ *Bare metal implementation of a UART DMA transmitter Driver
+ * If message is transmitted from memory to the uart data register (DMA), 
+ * than two LEDS (LED4 and LED6) are toggled (Interrupt) accordingly
  * if data is half or fully transmitted
- * After flashing the program into the board, the message is printed on RealTerm and
- * the LEDs are switched on
- * To test again, reset push button is to be pressed
- * External UART to USB adapter is used (AZ-Delivery)
- *
- * */
-
+ * 
+ ******************************************************************************
+ */
 #include "stm32f4xx.h"
 
 #include "UART.h"
